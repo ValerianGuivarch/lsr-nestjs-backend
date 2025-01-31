8c398f42d2ab938abef7eac5c1463e12d5876f8d
Valérian Guivarch <vguivarch@appstud.com>
2025-01-31 17:26:18 +0100

    test bloodline

--- src/main/data/database/bloodlines/DBBloodline.ts
+++ src/main/data/database/bloodlines/DBBloodline.ts
@@ -1,21 +1,24 @@
 import { DBProficiency } from '../proficiencies/DBProficiency'
 import { DBSkill } from '../skills/DBSkill'
-import { PrimaryGeneratedColumn, Column, Entity, ManyToMany } from 'typeorm'
+import { PrimaryColumn, Column, Entity, ManyToMany, JoinTable } from 'typeorm'
 
 @Entity()
 export class DBBloodline {
-  @PrimaryGeneratedColumn('uuid') // Clé primaire unique requise pour Directus
-  id: string
-
-  @Column({ type: 'varchar', unique: true })
+  @PrimaryColumn({ type: 'varchar' })
   name: string
 
   @Column({ type: 'varchar', default: '' })
   display: string
 
-  @ManyToMany(() => DBSkill, (skill) => skill.bloodlines)
+  @ManyToMany(() => DBSkill, (skill) => skill.bloodlines, {
+    cascade: ['remove']
+  })
+  @JoinTable()
   skills: DBSkill[]
 
-  @ManyToMany(() => DBProficiency, (proficiency) => proficiency.bloodlines)
+  @ManyToMany(() => DBProficiency, (proficiency) => proficiency.bloodlines, {
+    cascade: ['remove']
+  })
+  @JoinTable()
   proficiencies: DBProficiency[]
 }
